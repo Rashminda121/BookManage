8cccf26ef8e766e87fe7e1f99a7cfa5180eb4b52
Jayamuni Rashminda <rashminda121@gmail.com>
2024-05-29 19:20:17 +0530

    crud operation author

--- BookManagement/Views/Admin/Author.aspx
+++ BookManagement/Views/Admin/Author.aspx
@@ -33,15 +33,15 @@
                     </asp:DropDownList>
                 </div>
                 <div class="row">
-                    <asp:Label runat="server" ID="errMsg" class="text-danger"></asp:Label>
+                    <asp:Label runat="server" ID="errMsg" class="text-danger mb-3"></asp:Label>
                       <div class="col d-grid">
-                         <asp:Button Text="Update" runat="server" class="btn-warning btn-block btn" ID="editBtn"/>
+                         <asp:Button Text="Update" runat="server" class="btn-warning btn-block btn" ID="editBtn" OnClick="editBtn_Click"/>
                       </div>
                       <div class="col d-grid">
                         <asp:Button Text="Save" runat="server" class="btn-success btn-block btn" ID="saveBtn" OnClick="saveBtn_Click"/>
                       </div>
                       <div class="col d-grid">
-                        <asp:Button Text="Delete" runat="server" class="btn-danger btn-block btn" ID="deleteBtn"/>
+                        <asp:Button Text="Delete" runat="server" class="btn-danger btn-block btn" ID="deleteBtn" OnClick="deleteBtn_Click"/>
                       </div>
                     
                 </div>
@@ -51,7 +51,19 @@
 
             </div>
              <div class="col-md-8">
-                 <asp:GridView ID="AuthorsList" runat="server"></asp:GridView>
+                 <asp:GridView ID="AuthorsList" runat="server" class="table" CellPadding="4" ForeColor="#333333" GridLines="None" AutoGenerateSelectButton="True" OnSelectedIndexChanged="AuthorsList_SelectedIndexChanged">
+                     <AlternatingRowStyle BackColor="White" />
+                     <EditRowStyle BackColor="#7C6F57" />
+                     <FooterStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />
+                     <HeaderStyle BackColor="teal" Font-Bold="false" ForeColor="White" />
+                     <PagerStyle BackColor="#666666" ForeColor="White" HorizontalAlign="Center" />
+                     <RowStyle BackColor="#E3EAEB" />
+                     <SelectedRowStyle BackColor="#C5BBAF" Font-Bold="True" ForeColor="#333333" />
+                     <SortedAscendingCellStyle BackColor="#F8FAFA" />
+                     <SortedAscendingHeaderStyle BackColor="#246B61" />
+                     <SortedDescendingCellStyle BackColor="#D4DFE1" />
+                     <SortedDescendingHeaderStyle BackColor="#15524A" />
+                 </asp:GridView>
             </div>
         </div>
 </div>
--- BookManagement/Views/Admin/Author.aspx.cs
+++ BookManagement/Views/Admin/Author.aspx.cs
@@ -41,7 +41,9 @@ namespace BookManagement
                     Query =string.Format(Query, AName, Gender, Country);
                     Con.SetData(Query);
                     ShowAuthors() ;
-                    errMsg.Text = "Author Inserted!";
+                    errMsg.Text = "Author Inserted Successfully!";
+
+                    clearData();
 
                 }
 
@@ -50,6 +52,92 @@ namespace BookManagement
                 errMsg.Text = ex.Message;
             }
 
+        }
+
+        public void clearData()
+        {
+            ANameTb.Value = "";
+            GenCb.SelectedIndex = -1;
+            CountryCb.SelectedIndex = -1;
+        }
+
+        int Key = 0;
+
+        protected void AuthorsList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ANameTb.Value = AuthorsList.SelectedRow.Cells[2].Text;
+            GenCb.SelectedItem.Value = AuthorsList.SelectedRow.Cells[3].Text;
+            CountryCb.SelectedItem.Value = AuthorsList.SelectedRow.Cells[4].Text;
+            if (ANameTb.Value == "")
+            {
+                Key = 0;
+            }
+            else
+            {
+                Key = Convert.ToInt32(AuthorsList.SelectedRow.Cells[1].Text);
+            }
+        }
+
+        protected void editBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (ANameTb.Value == "" || GenCb.SelectedIndex == -1 || CountryCb.SelectedIndex == -1)
+                {
+                    errMsg.Text = "Missing Data!";
+                }
+                else
+                {
+                    string AName = ANameTb.Value;
+                    string Gender = GenCb.SelectedItem.ToString();
+                    string Country = CountryCb.SelectedItem.ToString();
+
+                    string Query = "update author set aname='{0}', agender='{1}' , acountry='{2}' where aid='{3}'  ";
+                    Query = string.Format(Query, AName, Gender, Country, AuthorsList.SelectedRow.Cells[1].Text);
+                    Con.SetData(Query);
+                    ShowAuthors();
+                    errMsg.Text = "Author Updated Successfully!";
+
+                    clearData();
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                errMsg.Text = ex.Message;
+            }
+        }
+
+        protected void deleteBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (ANameTb.Value == "" || GenCb.SelectedIndex == -1 || CountryCb.SelectedIndex == -1)
+                {
+                    errMsg.Text = "Please Select Data!";
+                }
+                else
+                {
+                    string AName = ANameTb.Value;
+                    string Gender = GenCb.SelectedItem.ToString();
+                    string Country = CountryCb.SelectedItem.ToString();
+
+                    string Query = "delete from author where aid='{0}'  ";
+                    Query = string.Format(Query, AuthorsList.SelectedRow.Cells[1].Text);
+                    Con.SetData(Query);
+                    ShowAuthors();
+                    errMsg.Text = "Author Deleted Successfully!";
+
+                    clearData();
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                errMsg.Text = ex.Message;
+            }
         }
     }
 }
